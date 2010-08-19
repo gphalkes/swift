ac560c579df8b87d337ffbc7b023aba426cfc263
Gertjan Halkes <git@ghalkes.nl>
2010-08-19 14:52:21 +0200

    Added local address listing to NAT test

--- nat_test.cpp
+++ nat_test.cpp
@@ -8,6 +8,15 @@
  */
 
 #include "swift.h"
+#ifdef _WIN32
+#include <iphlpapi.h>
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <errno.h>
+#include <netinet/in.h>
+#endif
 
 #define REQUEST_MAGIC 0x5a9e5fa1
 #define REPLY_MAGIC 0xa655c5d5
@@ -26,7 +35,7 @@ static sckrwecb_t callbacks(0, on_may_receive, on_may_send, NULL);
    the winsock library is first intialized. If we use Address type variables here, the
    lookup would be tried before that initialization, which fails... */
 //FIXME: Change addresses to actual addresses used in test (at least 2 should be provided!)
-static const char *servers[] = { "dutiep.st.ewi.tudelft.nl:18375" ,
+static const char *servers[] = { "dutigp.st.ewi.tudelft.nl:18375" ,
     "127.0.0.3:18375" };
 
 static void on_may_receive(SOCKET sock) {
@@ -75,7 +84,63 @@ static void on_may_send(SOCKET sock) {
     }
 }
 
+static void printAddresses(void) {
+#ifdef _WIN32
+    IP_ADAPTER_INFO *adapterInfo = NULL;
+    IP_ADAPTER_INFO *adapter = NULL;
+    DWORD retval = 0;
+    UINT i;
+    ULONG size = 0;
+
+    if ((retval = GetAdaptersInfo(adapterInfo, &size)) != ERROR_BUFFER_OVERFLOW) {
+        dprintf("ERROR: %d\n", (int) retval);
+        return;
+    }
+
+    adapterInfo = (IP_ADAPTER_INFO *) malloc(size);
+    if (adapterInfo == NULL) {
+        dprintf("ERROR: out of memory\n");
+        return;
+    }
+
+    if ((retval = GetAdaptersInfo(adapterInfo, &size)) == NO_ERROR) {
+        adapter = adapterInfo;
+        while (adapter) {
+            IP_ADDR_STRING *address;
+            for (address = &adapter->IpAddressList; address != NULL; address = address->Next) {
+                if (address->IpAddress.String[0] != 0)
+                    dprintf("ADDRESS: %s\n", address->IpAddress.String);
+            }
+            adapter = adapter->Next;
+        }
+    } else {
+        dprintf("ERROR: %d\n", (int) retval);
+    }
+    free(adapterInfo);
+#else
+    struct ifaddrs *addrs, *ptr;
+    if (getifaddrs(&addrs) < 0) {
+        dprintf("ERROR: %s\n", strerror(errno));
+        return;
+    }
+
+    for (ptr = addrs; ptr != NULL; ptr = ptr->ifa_next) {
+        if (ptr->ifa_addr->sa_family == AF_INET) {
+            dprintf("ADDRESS: %s\n", inet_ntoa(((struct sockaddr_in *) ptr->ifa_addr)->sin_addr));
+        }
+    }
+    freeifaddrs(addrs);
+#endif
+}
+
+
 void nat_test_update(void) {
+    static bool initialized;
+    if (!initialized) {
+        initialized = true;
+        printAddresses();
+    }
+
     if (tries < MAX_TRIES && NOW - test_start > 30 * TINT_SEC) {
         if (tries == 0) {
             Address any;
